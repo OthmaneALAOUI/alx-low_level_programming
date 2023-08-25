2ea1bc5673bc40457c8bc35e3c42809d13fbfc68
OthmaneALAOUI <othmane.alaoui.ismaili@gmail.com>
2023-08-24 18:17:07 -0700

    Update 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_lines - print the specified lines.
+ * Function is executed
+ */
+
+void print_lines(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I carry my house upon my back!\n");
+}

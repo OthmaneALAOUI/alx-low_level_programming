84035a28cc0522909a657fc2007c022ddf5fd48a
OthmaneALAOUI <othmane.alaoui.ismaili@gmail.com>
2023-08-25 17:45:38 -0700

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * first - print the specified lines.
  * Function is executed
  */
-
+void first(void) __attribute__ ((constructor));
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
